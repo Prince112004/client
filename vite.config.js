f26296ac34d95be72c34d68574a89d84bf4428cd
Prince <princechauhan112004@gmail.com>
2025-03-21 01:36:28 +0530

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080, // Add this for dev
+    port: 8080,
+    host: '0.0.0.0', // Ensure it is accessible externally
+    strictPort: true, // Ensures no other port is used
   },
   preview: {
-    port: 8080, // Add this for preview on Render
+    port: 8080,
+    host: '0.0.0.0',
+    strictPort: true,
+    allowedHosts: 'all', // Allow all hosts to access it
   },
 })
