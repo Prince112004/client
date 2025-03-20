4103d0d680df71533f620db36608b50ab3e75ffb
Prince <princechauhan112004@gmail.com>
2025-03-21 01:01:54 +0530

    Second Commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Ensure Vite knows where the root is
+  build: {
+    outDir: 'dist', // Ensure output goes to the dist folder
+  },
 })
