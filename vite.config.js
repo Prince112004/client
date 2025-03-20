0af4f5b536414d377d8c5016d8f325f1c36fca97
Prince <princechauhan112004@gmail.com>
2025-03-21 01:22:06 +0530

    Fix port issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Ensure Vite knows where the root is
-  build: {
-    outDir: 'dist', // Ensure output goes to the dist folder
+  server: {
+    port: 8080, // Add this for dev
+  },
+  preview: {
+    port: 8080, // Add this for preview on Render
   },
 })
